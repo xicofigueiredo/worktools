c51cc928c59956ceb8a2986a20928683f98dc6ed
Francisco Figueiredo <francisco-abf@hotmail.com>
2024-03-01 17:10:04 +0000

    nada

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,11 +1,9 @@
 import { Application } from "@hotwired/stimulus"
 
-
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
 
-// Stimulus.register("topics-done", TopicsDoneController)
 export { application }
