cf546f0f23a8f768d353323cc97ecc72dd085163
Francisco Figueiredo <francisco-abf@hotmail.com>
2024-04-19 03:03:35 +0100

    lws semi done

--- app/controllers/lws_timelines_controller.rb
+++ app/controllers/lws_timelines_controller.rb
@@ -15,7 +15,7 @@ class LwsTimelinesController < ApplicationController
     @lws_timeline.blocks_per_day = blocks_per_day(@lws_timeline)
     if @lws_timeline.save
       # Since user_topics_data is not used during creation, we can pass an empty hash or nil.
-      create_timelines_for_subjects(@lws_timeline, {})
+      create_timelines_for_subjects(@lws_timeline)
       redirect_to root_path, notice: 'LWS Timeline and associated timelines were successfully created.'
     else
       render :new
@@ -28,9 +28,8 @@ class LwsTimelinesController < ApplicationController
   def update
     ActiveRecord::Base.transaction do
       if @lws_timeline.update(lws_timeline_params)
-        user_topics_data = save_user_topics_data(@lws_timeline)  # Assuming this method returns the needed data
-        recreate_timelines_for_subjects(@lws_timeline, user_topics_data)
-        redirect_to lws_timelines_path, notice: 'LWS Timeline and associated timelines were successfully updated.'
+        recreate_timelines_for_subjects(@lws_timeline)
+        redirect_to root_path, notice: 'LWS Timeline and associated timelines were successfully updated.'
       else
         render :edit
       end
@@ -42,7 +41,7 @@ class LwsTimelinesController < ApplicationController
 
   def destroy
     @lws_timeline.destroy
-    redirect_to lws_timelines_url, notice: 'LWS Timeline was successfully destroyed.'
+    redirect_to root_path, notice: 'LWS Timeline was successfully destroyed.'
   end
 
   private
@@ -66,7 +65,7 @@ class LwsTimelinesController < ApplicationController
     blocks / days
   end
 
-  def create_timelines_for_subjects(lws_timeline, user_topics_data = {})
+  def create_timelines_for_subjects(lws_timeline)
     subjects = Subject.where(category: lws_timeline.level)
     return if subjects.empty?
 
@@ -92,55 +91,55 @@ class LwsTimelinesController < ApplicationController
       )
 
       # Here, you would restore user_topics data if applicable
-      restore_user_topics(user_topics_data)
+      # restore_user_topics(user_topics_data)
     end
   end
 
 
-  def restore_user_topics(user_topics_data)
-    user_topics_data.each do |ut_id, done_status|
-      # Find the topic ID associated with this user topic.
-      topic_id = Topic.find(ut_id).topic_id
+  # def restore_user_topics(user_topics_data)
+  #   user_topics_data.each do |ut_id, done_status|
+  #     # Find the topic ID associated with this user topic.
+  #     topic_id = Topic.find(ut_id).topic_id
 
-      # Ensure the new timeline is associated with this topic.
-      timeline = Timeline.find_by(user_id: current_user.id, subject_id: Subject.find_by(topic_id: topic_id))
+  #     # Ensure the new timeline is associated with this topic.
+  #     timeline = Timeline.find_by(user_id: current_user.id, subject_id: Subject.find_by(topic_id: topic_id))
 
-      # Recreate the user topic with the associated new timeline and old done status.
-      UserTopic.create!(
-        topic_id: topic_id,
-        user_id: current_user.id,
-        timeline_id: timeline.id,
-        done: done_status
-      )
-    end
-  end
+  #     # Recreate the user topic with the associated new timeline and old done status.
+  #     UserTopic.create!(
+  #       topic_id: topic_id,
+  #       user_id: current_user.id,
+  #       timeline_id: timeline.id,
+  #       done: done_status
+  #     )
+  #   end
+  # end
 
-  def recreate_timelines_for_subjects(lws_timeline, user_topics_data)
+  def recreate_timelines_for_subjects(lws_timeline)
     # Delete existing timelines
     lws_timeline.timelines.destroy_all
     # Recreate timelines and restore user topics
     create_timelines_for_subjects(lws_timeline)
-    restore_user_topics(user_topics_data)
+    # restore_user_topics(user_topics_data)
   end
 
 
 
-  def save_user_topics_data(lws_timeline)
-    user_topics_data = {}
+  # def save_user_topics_data(lws_timeline)
+  #   user_topics_data = {}
 
-    # Assuming subjects are grouped by a field like `level` in LwsTimeline which matches with subjects
-    subject_ids = Subject.where(category: lws_timeline.level).pluck(:id)
+  #   # Assuming subjects are grouped by a field like `level` in LwsTimeline which matches with subjects
+  #   subject_ids = Subject.where(category: lws_timeline.level).pluck(:id)
 
-    user_topics = UserTopic.joins(topic: :subject)
-                           .where(user_id: lws_timeline.user_id, subjects: { id: subject_ids })
-                           .where("topics.created_at BETWEEN ? AND ?", lws_timeline.start_date, lws_timeline.end_date)
+  #   user_topics = UserTopic.joins(topic: :subject)
+  #                          .where(user_id: lws_timeline.user_id, subjects: { id: subject_ids })
+  #                          .where("topics.created_at BETWEEN ? AND ?", lws_timeline.start_date, lws_timeline.end_date)
 
-    user_topics.each do |ut|
-      user_topics_data[ut.topic_id] = ut.done
-    end
+  #   user_topics.each do |ut|
+  #     user_topics_data[ut.topic_id] = ut.done
+  #   end
 
-    user_topics_data
-  end
+  #   user_topics_data
+  # end
 
 
 
