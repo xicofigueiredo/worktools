3c33cb57aa125405c83d0e61e2c95f123bbfe3c5
Francisco Figueiredo <francisco-abf@hotmail.com>
2024-05-03 13:11:39 +0100

    profile update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -28,35 +28,31 @@ class PagesController < ApplicationController
   end
 
   def profile
-    @hubs = current_user.hubs
-    @timelines = current_user.timelines_sorted_by_balance
+    if current_user.role != "lc"
+      @learner = current_user
+      @learner_flag = @learner.learner_flag
+      @timelines = @learner.timelines
+      @current_sprint = Sprint.where("start_date <= ? AND end_date >= ?", Date.today, Date.today).first
+      @current_sprint_weeks = @current_sprint.weeks.order(:start_date)
+      @sprint_goals = @learner.sprint_goals.find_by(sprint: @current_sprint)
+      @skills = @sprint_goals&.skills
+      @communities = @sprint_goals&.communities
+      @hub_lcs = @learner.hubs.first.users.where(role: 'lc')
 
-    @overall_progress = 0
-    @overall_progress_expected = 0
-    @timelines.each do |timeline|
-      @overall_progress += timeline.progress
-      @overall_progress_expected += timeline.expected_progress
-    end
-    if @timelines.count > 0
-      @overall_progress = @overall_progress / @timelines.count
-      @overall_progress_expected = @overall_progress_expected / @timelines.count
-    else
-      @overall_progress = 0
-      @overall_progress_expected = 0
-    end
+      @weekly_goals_percentage = @current_sprint.count_weekly_goals_total(@learner)
+      @kdas_percentage = @current_sprint.count_kdas_total(@learner)
 
-    @activities = []
-    current_sprint = Sprint.where("start_date <= ? AND end_date >= ?", Date.today, Date.today).first
-    current_user.sprint_goals.all.where(sprint: current_sprint).each do |sprint|
-      if sprint.skills.count.positive?
-        sprint.skills.each do |skill|
-          @activities << [skill.extracurricular, skill.smartgoals]
-        end
-      end
-      if sprint.communities.count.positive?
-        sprint.communities.each do |community|
-          @activities << [community.involved, community.smartgoals]
-        end
+      @has_exam_date = @timelines.any? { |timeline| timeline.exam_date.present? }
+
+      @current_week = Week.find_by("start_date <= ? AND end_date >= ?", Date.today, Date.today)
+
+      @has_mock50 = @timelines.any? { |timeline| timeline.mock50.present? }
+
+      @has_mock100 = @timelines.any? { |timeline| timeline.mock50.present? }
+
+      get_kda_averages(@learner.kdas)
+      unless @learner
+        redirect_to some_fallback_path, alert: "Learner not found."
       end
     end
   end
