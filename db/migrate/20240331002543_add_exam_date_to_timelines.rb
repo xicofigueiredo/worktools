f9966aef6331adc6e2a04a1b8b285346cb0a42ee
Francisco Figueiredo <francisco-abf@hotmail.com>
2024-05-16 13:45:30 +0100

    exam date null

--- db/migrate/20240331002543_add_exam_date_to_timelines.rb
+++ db/migrate/20240331002543_add_exam_date_to_timelines.rb
@@ -1,6 +1,6 @@
 class AddExamDateToTimelines < ActiveRecord::Migration[7.0]
   def change
     remove_column :timelines, :exam_season, :datatype
-    add_reference :timelines, :exam_date, null: false, foreign_key: true
+    add_reference :timelines, :exam_date, null: true, foreign_key: true
   end
 end
