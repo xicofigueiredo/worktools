52a59c96039a5dd00bcc45fb76f2701b0bf684bf
Daniel Gaspar <daniel.s.gaspar@gmail.com>
2024-05-21 21:06:28 +0100

    fix: Timelines Stimulus Controller

--- app/javascript/controllers/toggle_done_controller.js
+++ app/javascript/controllers/toggle_done_controller.js
@@ -4,9 +4,16 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["checkbox"]; // Assuming you have 'data-toggle-done-target="checkbox"' on your checkbox
 
+  connect() {
+    console.log("connected");
+  }
+
   toggle(event) {
-    const userTopicId = this.checkboxTarget.dataset.userTopicId; // Correctly access the userTopicId
-    const timelineId = this.checkboxTarget.dataset.timelineId;
+    const checkbox = event.target;
+    const userTopicId = checkbox.dataset.userTopicId; // Correctly access the userTopicId
+    const timelineId = checkbox.dataset.timelineId;
+
+    console.log("Called toggle with: ", { userTopicId, timelineId });
 
     fetch(`/user_topics/${userTopicId}/toggle_done`, {
       method: "PATCH",
