4c06630029065d783734f606a790bdb7babf7caa
Francisco Figueiredo <francisco-abf@hotmail.com>
2024-05-26 13:22:38 +0100

    Revert "Merge pull request #42 from xicofigueiredo/fix/sprint-goals-edit-knowledges"
    This reverts commit be4228ce9099b0a2d8b6fc8fcf0e3b961bbd6561, reversing
    changes made to f8c2a4825110559189645453ca16b36236cc4c1f.

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -4,7 +4,6 @@ export default class extends Controller {
   static targets = [
     "skillsContainer",
     "communitiesContainer",
-    "knowledgesContainer",
     "template",
     "form",
     "submit",
@@ -15,15 +14,6 @@ export default class extends Controller {
     this.communityId = this.element.dataset.communityId;
     this.deletedCommunityIds = [];
     this.deletedSkillsIds = [];
-    this.deletedKnowledgeIds = [];
-  }
-
-  formatDate(dateString) {
-    const date = new Date(dateString);
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-based
-    const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
   }
 
   getCSRFToken() {
@@ -34,7 +24,7 @@ export default class extends Controller {
 
   addRow(event) {
     event.preventDefault();
-    const kind = event.target.dataset.kind; // 'skills' or 'communities' or 'knowledges'
+    const kind = event.target.dataset.kind; // 'skills' or 'communities'
     const templates = this.templateTargets.filter(
       (t) => t.dataset.kind === kind
     );
@@ -51,8 +41,6 @@ export default class extends Controller {
           "beforeend",
           content
         );
-      } else if (kind === "knowledges") {
-        this.knowledgesContainerTarget.insertAdjacentHTML("beforeend", content);
       }
     } else {
       console.error("Template for", kind, "not found.");
@@ -68,56 +56,13 @@ export default class extends Controller {
     if (kind === "communities") {
       const communityId = button.dataset.communityId;
       this.deletedCommunityIds.push(communityId);
-    } else if (kind === "skills") {
+    } else {
       const skillId = button.dataset.skillId;
       this.deletedSkillsIds.push(skillId);
-    } else if (kind === "knowledges") {
-      const knowledgeId = button.dataset.knowledgeId;
-      this.deletedKnowledgeIds.push(knowledgeId);
     }
     if (row) {
       row.remove();
     }
-  }
-
-  createKnowledge(event) {
-    const selectedValue = event.target.value;
-    const [subjectName, examSeason, mock50, mock100] =
-      selectedValue.split("||");
-
-    const formattedMock50 = this.formatDate(mock50);
-    const formattedMock100 = this.formatDate(mock100);
-
-    const fontSize = "style='font-size: 13px;'";
-
-    const row = event.target.closest("tr");
-
-    row.querySelector('input[name$="[subject_name]"]').value = subjectName;
-    row.querySelector('input[name$="[exam_season]"]').value = "N/A";
-    row.querySelector('input[name$="[mock50]"]').value = formattedMock50;
-    row.querySelector('input[name$="[mock100]"]').value = formattedMock100;
-
-    row.querySelector('input[name$="[subject_name]"]').disabled = false;
-    row.querySelector('input[name$="[exam_season]"]').disabled = false;
-    row.querySelector('input[name$="[mock50]"]').disabled = false;
-    row.querySelector('input[name$="[mock100]"]').disabled = false;
-    row.querySelector('textarea[name$="[difficulties]"]').disabled = false;
-    row.querySelector('textarea[name$="[plan]"]').disabled = false;
-
-    const subjectCell = row.querySelector("[data-subject-cell]");
-    subjectCell.innerHTML = `<p ${fontSize}>${subjectName}</p>`;
-
-    const sprintGoalCell = row.querySelector("[data-sprint-goal-cell]");
-    sprintGoalCell.innerHTML = `<p ${fontSize}>N/A</p>`;
-
-    const examSeasonCell = row.querySelector("[data-exam-season-cell]");
-    examSeasonCell.innerHTML = `<p ${fontSize}>N/A</p>`;
-
-    const mock50Cell = row.querySelector("[data-mock50-cell]");
-    mock50Cell.innerHTML = `<p ${fontSize}>${formattedMock50}</p>`;
-
-    const mock100Cell = row.querySelector("[data-mock100-cell]");
-    mock100Cell.innerHTML = `<p ${fontSize}>${formattedMock100}</p>`;
   }
 
   async submit(event) {
@@ -125,8 +70,7 @@ export default class extends Controller {
     const sprintGoalId = event.target.dataset.sprintGoalId;
     const sprintGoalDate = event.target.dataset.sprintGoalDate;
     const deletedCommunityIds = this.deletedCommunityIds;
-    const deletedSkillIds = this.deletedSkillsIds;
-    const deletedKnowledgeIds = this.deletedKnowledgeIds;
+    const deletedSkillsIds = this.deletedSkillsIds;
     const token = this.getCSRFToken();
 
     const form = this.formTarget;
@@ -135,11 +79,7 @@ export default class extends Controller {
     this.submitTarget.innerHTML =
       '<button class="btn btn-primary" style="border-radius: 10px" disabled>Saving...</button>';
 
-    if (
-      deletedCommunityIds.length > 0 ||
-      deletedSkillIds.length > 0 ||
-      deletedKnowledgeIds.length > 0
-    ) {
+    if (deletedCommunityIds.length > 0 || deletedSkillsIds > 0) {
       try {
         const response = await fetch("/sprint_goals/bulk_destroy", {
           method: "POST",
@@ -149,8 +89,7 @@ export default class extends Controller {
           },
           body: JSON.stringify({
             deleted_communities_ids: deletedCommunityIds,
-            deleted_skills_ids: deletedSkillIds,
-            deleted_knowledges_ids: deletedKnowledgeIds,
+            deleted_skills_ids: deletedSkillsIds,
           }),
         });
 
