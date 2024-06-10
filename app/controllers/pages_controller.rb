0b4f5b66db23bf3de66397dc7cfd4c3750804718
Daniel Gaspar <daniel.s.gaspar@gmail.com>
2024-06-10 18:42:51 +0100

    Rebased and refactored notes section + solved no notes bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -166,7 +166,7 @@ class PagesController < ApplicationController
   def update_weekly_goal(weekly_goal, week, learner, date)
     render turbo_stream:
       turbo_stream.replace("lp_weekly_goal",
-                            partial: "pages/weekly_goals",
+                            partial: "pages/partials/weekly_goals",
                             locals: {weekly_goal: weekly_goal,
                                     current_week: week,
                                     learner: learner,
