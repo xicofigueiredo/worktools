0f2a5ab5332ac0951f3731138da8b4c1a1e4fdc2
Francisco Figueiredo <francisco-abf@hotmail.com>
2024-11-20 12:12:57 +0000

    difference protection

--- app/models/report_knowledge.rb
+++ app/models/report_knowledge.rb
@@ -2,5 +2,7 @@ class ReportKnowledge < ApplicationRecord
   belongs_to :report
 
   validates :subject_name, presence: true
-  
+  validates :progress, presence: true
+  validates :difference, presence: true
+
 end
