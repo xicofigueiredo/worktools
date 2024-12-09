27334ef541691d56c242b79a10494b970024d008
Francisco Figueiredo <francisco-abf@hotmail.com>
2024-12-09 15:02:54 +0000

    outlook update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,13 +4,15 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.office365.com',
     port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+    authentication:       'login',
+    user_name:            ENV['OUTLOOK_USERNAME'],
+    password:             ENV['OUTLOOK_PASSWORD'],
+    open_timeout:         30,
+    read_timeout:         300,
+    enable_starttls_auto: true}
+
     config.action_mailer.default_url_options = {:host =>"http://worktools.site"}  # Settings specified here will take precedence over those in config/application.rb.
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.perform_deliveries = true
