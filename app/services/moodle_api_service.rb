2b8725b5aab88bb1e0fdd2f3af14bed63a0bd01e
Francisco Figueiredo <francisco-abf@hotmail.com>
2025-01-08 17:15:52 +0000

    first step

--- app/services/moodle_api_service.rb
+++ app/services/moodle_api_service.rb
@@ -1,24 +1,30 @@
-# app/services/moodle_api_service.rb
 class MoodleApiService
   def initialize
     @base_url = ENV['MOODLE_API_BASE_URL']
-    @token = ENV['MOODLE_API_TOKEN']
+    @token    = ENV['MOODLE_API_TOKEN']
   end
 
   def call(function_name, params = {})
-    params.merge!({
+    base_params = {
       wstoken: @token,
       wsfunction: function_name,
       moodlewsrestformat: 'json'
-    })
+    }.merge(params)
 
-    response = RestClient.get(@base_url, { params: params })
+    # Use POST for complex or any multi-dimensional data
+    response = RestClient.post(@base_url, base_params)
     JSON.parse(response.body)
   rescue RestClient::ExceptionWithResponse => e
     { error: e.response }
   end
 
-  def get_courses
-    call('gradereport_overview_get_course_grades', {})
+  # 1) Find a course by shortname
+  def find_course_by_shortname(shortname)
+    call('core_course_get_courses_by_field', { field: 'shortname', value: shortname })
+  end
+
+  # 2) Get enrolled users for a given course id
+  def get_enrolled_users(course_id)
+    call('core_enrol_get_enrolled_users', { courseid: course_id })
   end
 end
