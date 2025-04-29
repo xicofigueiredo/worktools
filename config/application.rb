bedc3bce1046e9168f70cb1823662e1203872dd6
Francisco Figueiredo <francisco-abf@hotmail.com>
2025-04-29 10:09:12 +0100

    upgrade first things

--- config/application.rb
+++ config/application.rb
@@ -8,20 +8,20 @@ Bundler.require(*Rails.groups)
 
 module Worktools
   class Application < Rails::Application
-    config.generators do |generate|
-      generate.assets false
-      generate.helper false
-      generate.test_framework :test_unit, fixture: false
-    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Lisbon"
+    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,25 +1,12 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-
-  config.action_mailer.default_url_options = { host: 'bravegenerationacademy.com' }
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.office365.com',
-    port:                 587,
-    authentication:       'login',
-    user_name:            ENV['OUTLOOK_USERNAME'],
-    password:             ENV['OUTLOOK_PASSWORD'],
-    enable_starttls_auto: true}
-    config.action_mailer.default_url_options = {:host =>'localhost:3000'}  # Settings specified here will take precedence over those in config/application.rb.
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.perform_deliveries = true
+  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.enable_reloading = true
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -69,6 +56,9 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Highlight code that enqueued background job in logs.
+  config.active_job.verbose_enqueue_logs = true
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
@@ -80,10 +70,13 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  #
   config.after_initialize do
     Bullet.enable = true
     Bullet.rails_logger = true
     Bullet.console = true
   end
+
+  # Raise error when a before_action's only/except options reference missing actions
+
+  config.action_controller.raise_on_missing_callback_actions = true
 end
