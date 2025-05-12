e0b40e48e4587461b51fd42db50f788015c09f11
Francisco Figueiredo <francisco-abf@hotmail.com>
2025-05-12 15:41:38 +0100

    notifcations fix and skills on reports update

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,9 +3,18 @@ class Skill < ApplicationRecord
   has_one :report_activity, dependent: :destroy
 
   after_create :create_report_activity
+  after_update :update_report_activity
 
   def create_report_activity
     report = Report.find_by(user: self.sprint_goal.user, sprint: self.sprint_goal.sprint)
     ReportActivity.create(report: report, activity: self.extracurricular, goal: self.smartgoals, skill_id: self.id)
+  end
+
+  def update_report_activity
+    return unless report_activity.present?
+    report_activity.update(
+      activity: self.extracurricular,
+      goal: self.smartgoals
+    )
   end
 end
