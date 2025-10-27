b14029713cae9b99de27921d228d5f3bb5409309
Francisco Figueiredo <francisco-abf@hotmail.com>
2025-10-27 21:34:45 +0800

    michelle as rm for exams

--- app/helpers/exam_enrolls_helper.rb
+++ app/helpers/exam_enrolls_helper.rb
@@ -13,7 +13,7 @@ module ExamEnrollsHelper
        'extension_cm_approval',
        'extension_cm_comment']
     when 'lc'
-      if current_user.hubs.count > 5 || current_user.id == 247
+      if current_user.hubs.count > 5 || current_user.id == 247 || current_user.id == 99
       ['pre_registration_exception_dc_approval',
        'pre_registration_exception_dc_comment',
        'failed_mock_exception_dc_approval',
