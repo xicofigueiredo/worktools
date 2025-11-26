b1d1d04d42437053bbd7d26837a45f917a30f766
Francisco Figueiredo <francisco-abf@hotmail.com>
2025-11-26 16:31:48 +0000

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -183,7 +183,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_11_25_200039) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.bigint "hub_id", null: false
+    t.bigint "hub_id"
     t.bigint "week_id"
     t.index ["day"], name: "index_consent_activities_on_day"
     t.index ["hub_id"], name: "index_consent_activities_on_hub_id"
@@ -286,7 +286,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_11_25_200039) do
     t.string "subject_name"
     t.string "code"
     t.string "qualification"
-    t.boolean "progress_cut_off", default: false
     t.string "mock_results"
     t.string "bga_exam_centre"
     t.string "exam_board"
@@ -323,6 +322,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_11_25_200039) do
     t.integer "learning_coach_ids", default: [], array: true
     t.integer "timeline_id"
     t.string "specific_papers"
+    t.boolean "progress_cut_off", default: false
     t.string "personalized_exam_date"
     t.string "paper1"
     t.string "paper2"
@@ -349,6 +349,11 @@ ActiveRecord::Schema[7.0].define(version: 2025_11_25_200039) do
     t.string "currency", default: "EUR"
     t.integer "number_of_subjects", default: 0, null: false
     t.index ["user_id"], name: "index_exam_finances_on_user_id"
+  end
+
+  create_table "excel_imports", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "form_interrogation_joins", force: :cascade do |t|
@@ -1025,6 +1030,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_11_25_200039) do
     t.integer "progress"
     t.bigint "exam_date_id"
     t.boolean "anulado"
+    t.bigint "lws_timeline_id"
     t.datetime "mock50"
     t.datetime "mock100"
     t.string "personalized_name"
@@ -1032,6 +1038,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_11_25_200039) do
     t.boolean "hidden", default: false
     t.integer "difference"
     t.index ["exam_date_id"], name: "index_timelines_on_exam_date_id"
+    t.index ["lws_timeline_id"], name: "index_timelines_on_lws_timeline_id"
     t.index ["subject_id"], name: "index_timelines_on_subject_id"
     t.index ["user_id"], name: "index_timelines_on_user_id"
   end
@@ -1086,9 +1093,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_11_25_200039) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "full_name"
@@ -1098,6 +1102,10 @@ ActiveRecord::Schema[7.0].define(version: 2025_11_25_200039) do
     t.date "birthday"
     t.string "nationality"
     t.string "profile_pic"
+    t.datetime "last_login_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.boolean "deactivate", default: false
     t.bigint "moodle_id"
@@ -1237,7 +1245,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_11_25_200039) do
   add_foreign_key "kdas", "weeks"
   add_foreign_key "knowledges", "moodle_timelines"
   add_foreign_key "knowledges", "sprint_goals"
-  add_foreign_key "knowledges", "timelines", on_delete: :cascade
   add_foreign_key "learner_documents", "learner_infos"
   add_foreign_key "learner_finances", "learner_infos"
   add_foreign_key "learner_flags", "users"
@@ -1285,7 +1292,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_11_25_200039) do
   add_foreign_key "timeline_progresses", "timelines"
   add_foreign_key "timeline_progresses", "weeks"
   add_foreign_key "timelines", "exam_dates"
-  add_foreign_key "timelines", "subjects"
   add_foreign_key "timelines", "users"
   add_foreign_key "topics", "subjects"
   add_foreign_key "tuesday_slots", "users", column: "lc_id"
